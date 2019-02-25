937b0fdc0e209f83c91ce9854bb0f3c22632ab30
Arnobw <arnobw@hotmail.com>
2019-02-25 14:29:55 +0100

    yeee

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
+
 
 
 
